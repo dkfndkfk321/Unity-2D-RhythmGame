3fa461b8cfdf7f459fcddb51280fbc6f726dfe5a
dkfndkfk321 <48555199+dkfndkfk321@users.noreply.github.com>
2019-03-25 17:45:34 +0900

    에셋 추가 및 게임 화면 구성

---
+++ Unity2DRythmGame/Assets/Scripts/BackgroundBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundBehaviour : MonoBehaviour
+{
+
+    public GameObject background;
+    private SpriteRenderer backgroundSpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundSpriteRenderer = background.GetComponent<SpriteRenderer>();
+        StartCoroutine(FadeOut(backgroundSpriteRenderer, 0.005f));
+
+        IEnumerator FadeOut(SpriteRenderer spriteRenderer, float amount)
+        {
+            Color color = spriteRenderer.color;
+            while(color.a > 0)
+            {
+                color.a -= amount;
+                spriteRenderer.color = color;
+                yield return new WaitForSeconds(amount);
+            }
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
